78eac8ce0d0c472fd4cc5ce972cd6e5ff601e6d6
Steven Petrov <91495882+StevenPetrov@users.noreply.github.com>
2022-10-04 00:10:06 +0300

    more updates

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -16,7 +16,10 @@ SECRET_KEY = 'django-insecure-2oiar3$albv_ub3exqx!!6!395y7o@i$ptp#q(bdjn@q=o*8cv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
